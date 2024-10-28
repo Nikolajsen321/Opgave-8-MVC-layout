db6eacef7cc9c063652f3b3bf37ca4e55610495d
Nikolaj <nikolajkc@live.dk>
2024-10-28 06:56:32 +0100

    Add project files.

---
+++ Opgave 8 layout/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Opgave_8_layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Sale()
+        {
+            return View();
+        }
+
+        public IActionResult MoreSales()
+        {
+            return View();
+        }
+
+        public IActionResult Buy()
+        {
+            return View();
+        }
+
+        public IActionResult BuyAndSale()
+        {
+            return View();
+        }
+    }
+}
